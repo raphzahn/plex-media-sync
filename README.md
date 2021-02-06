a658b242cc88e097a07c886eb90428261cd4e553
Raphael Zahner <r.zahner@voegeli.ch>
2021-02-06 19:30:00 +0100

    Finish script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Plex Media Sync
+
+This Script will allow to copy Media form a External Disk to your Plex Media Server
---
+++ sync.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+EXTERNAL_DISK="/Volumes/Elements/Media/" #Change to the name of your External Disk an die Lociation of the Media
+PLEX_MEDIA_DIRECTORY="/Media/" # Change to the path of our Media for the Plex Server
+
+# Check if External Disk is Mounted
+if [ -d $EXTERNAL_DISK ]
+then
+    # Check if Local Media Storage is Existing
+    if [ -d $PLEX_MEDIA_DIRECTORY ]
+    then
+        # Copy Movies
+        cp -r "${EXTERNAL_DISK}Movies/" "${PLEX_MEDIA_DIRECTORY}Movies/"
+
+        # Copy TV Series
+        cp -r "${EXTERNAL_DISK}TV Serien/" "${PLEX_MEDIA_DIRECTORY}TV Serien/"
+    else
+        say "Can't find Plex Directory"
+    fi
+else
+    say "Can't find External Disk"
+fi
